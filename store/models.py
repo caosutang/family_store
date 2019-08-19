9176368bf0ec4d581ccdd02709113d07413f2793
caosutang <minhhuyspkt@gmail.com>
2019-08-19 23:54:15 +0700

    Update home.html

--- store/models.py
+++ store/models.py
@@ -19,9 +19,12 @@ LABEL_CHOICES = (
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField()
 
     def __str__(self):
         return self.title
@@ -48,4 +51,3 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
-
